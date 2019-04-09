96cdd1f6ca24dcf24b5cb803bebd7f35a4b4914d
Ильяшук Данил <danil.ilyashuk@gmail.com>
2019-04-09 17:18:51 +0300

    add map view

--- android/app/src/main/java/com/dd/routing/MainActivity.kt
+++ android/app/src/main/java/com/dd/routing/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.dd.routing
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.preference.PreferenceManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Configuration.getInstance().load(applicationContext, PreferenceManager.getDefaultSharedPreferences(applicationContext))
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.setMultiTouchControls(true)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        map.onPause()
     }
 }

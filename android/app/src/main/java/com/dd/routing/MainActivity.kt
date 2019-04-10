cb0253c6b058b806ef6ff775e558008c88b41835
Ильяшук Данил <danil.ilyashuk@gmail.com>
2019-04-10 10:09:46 +0300

    add navigation menu

--- android/app/src/main/java/com/dd/routing/MainActivity.kt
+++ android/app/src/main/java/com/dd/routing/MainActivity.kt
@@ -3,8 +3,12 @@ package com.dd.routing
 
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main_content.*
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 
@@ -13,10 +17,60 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Configuration.getInstance().load(applicationContext, PreferenceManager.getDefaultSharedPreferences(applicationContext))
-        map.setTileSource(TileSourceFactory.MAPNIK)
-        map.setMultiTouchControls(true)
+        //Setup mapView
+        Configuration.getInstance()
+            .load(applicationContext, PreferenceManager.getDefaultSharedPreferences(applicationContext))
+        map.apply {
+            setTileSource(TileSourceFactory.MAPNIK)
+            setMultiTouchControls(true)
+        }
+
+
+        navigation_view.setNavigationItemSelectedListener { menuItem ->
+            //TODO: implement behaviour
+            when (menuItem.itemId) {
+                R.id.nav_available_area -> {
+                }
+                R.id.nav_data -> {
+                }
+                R.id.nav_about -> {
+                }
+                R.id.nav_exit -> {
+                }
+                else -> {
+                }
+            }
+            drawer_layout.closeDrawers()
+            true
+        }
+
+
+        //TODO: добавить поведение если это понадобится или просто удалить
+        drawer_layout.addDrawerListener(
+            object : DrawerLayout.DrawerListener {
+                override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                    // Respond when the drawer's position changes
+                }
+
+                override fun onDrawerOpened(drawerView: View) {
+                    // Respond when the drawer is opened
+                }
+
+                override fun onDrawerClosed(drawerView: View) {
+                    // Respond when the drawer is closed
+                }
+
+                override fun onDrawerStateChanged(newState: Int) {
+                    // Respond when the drawer motion state changes
+                }
+            }
+        )
+
+//        testButton.setOnClickListener {
+//            drawer_layout.openDrawer(GravityCompat.START)
+//        }
     }
+
 
     override fun onResume() {
         super.onResume()

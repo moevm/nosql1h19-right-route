d28fb8b97a732be8a8f2a121648d8a7cf9457c16
Ильяшук Данил <danil.ilyashuk@gmail.com>
2019-05-19 17:39:12 +0300

    data activity check button

--- android/app/src/main/java/com/dd/routing/DataActivity.kt
+++ android/app/src/main/java/com/dd/routing/DataActivity.kt
@@ -2,15 +2,16 @@ package com.dd.routing
 
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.DefaultRetryPolicy
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import kotlinx.android.synthetic.main.activity_data.*
 import kotlinx.android.synthetic.main.activity_main_content.map
+import org.json.JSONObject
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
@@ -55,7 +56,7 @@ class DataActivity : AppCompatActivity() {
             val jsonObjectRequest = JsonObjectRequest(
                 Request.Method.GET, urlBuilder.toString(), null,
                 Response.Listener {
-                    Toast.makeText(this, it.getString("msg"), Toast.LENGTH_LONG).show()
+                    saveOperationId(it)
                 },
                 Response.ErrorListener {
                     Toast.makeText(this, "Backup error", Toast.LENGTH_SHORT).show()
@@ -70,7 +71,7 @@ class DataActivity : AppCompatActivity() {
             val jsonObjectRequest = JsonObjectRequest(
                 Request.Method.GET, urlBuilder.toString(), null,
                 Response.Listener {
-                    Toast.makeText(this, it.getString("msg"), Toast.LENGTH_LONG).show()
+                    saveOperationId(it)
                 },
                 Response.ErrorListener {
                     Toast.makeText(this, "Restore error", Toast.LENGTH_SHORT).show()
@@ -90,7 +91,7 @@ class DataActivity : AppCompatActivity() {
             val jsonObjectRequest = JsonObjectRequest(
                 Request.Method.GET, urlBuilder.toString(), null,
                 Response.Listener {
-                    Toast.makeText(this, it.getString("msg"), Toast.LENGTH_LONG).show()
+                    saveOperationId(it)
                 },
                 Response.ErrorListener {
                     Toast.makeText(this, "Import error", Toast.LENGTH_SHORT).show()
@@ -98,5 +99,43 @@ class DataActivity : AppCompatActivity() {
             )
             VolleyQueue.getInstance(this).addToRequestQueue(jsonObjectRequest)
         }
+    }
+
+    private fun saveOperationId(json: JSONObject) {
+        Toast.makeText(this, json.getString("msg"), Toast.LENGTH_SHORT).show()
+        if (json.getBoolean("error")) {
+            return
+        }
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        preferences.edit().apply {
+            putString("operationId", json.getJSONObject("data").getString("id"))
+            apply()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actionbar_status_button, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.info_button) {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+            val operationId = preferences.getString("operationId", VolleyQueue.serverUrl) ?: return true
+            val urlBuilder = StringBuilder(getServerUrl(this))
+                .append("/api/0.5/check?id=$operationId")
+            val jsonObjectRequest = JsonObjectRequest(
+                Request.Method.GET, urlBuilder.toString(), null,
+                Response.Listener {
+                    Toast.makeText(this, it.getString("msg"), Toast.LENGTH_SHORT).show()
+                },
+                Response.ErrorListener {
+                    Toast.makeText(this, "Info error", Toast.LENGTH_SHORT).show()
+                }
+            )
+            VolleyQueue.getInstance(this).addToRequestQueue(jsonObjectRequest)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
